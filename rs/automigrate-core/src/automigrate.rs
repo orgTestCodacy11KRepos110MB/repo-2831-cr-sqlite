d18ab1ce0246087b9a72433d286e96bf2400bacd
Matt <1009003+tantaman@users.noreply.github.com>
2023-01-30 17:09:12 -0500

    start on automigration support

--- rs/automigrate-core/src/automigrate.rs
+++ rs/automigrate-core/src/automigrate.rs
@@ -1,36 +1,13 @@
+use sqlite_nostd::ResultCode;
+
 extern crate alloc;
 
-use alloc::string::String;
-use core::ffi::c_char;
-use core::slice;
 use sqlite_nostd as sqlite;
 
-use sqlite::Connection;
-use sqlite::Context;
-use sqlite::Value;
-
-pub extern "C" fn crsql_automigrate(
-    ctx: *mut sqlite::context,
-    argc: i32,
-    argv: *mut *mut sqlite::value,
-) {
-    let args = sqlite::args!(argc, argv);
-    if argc != 1 {
-        ctx.result_error("expected 1 argument");
-        return;
-    }
-
-    let schema = args[0].text();
-
-    // let schema_version = pull_schema_version(schema);
-    let db = sqlite::open(sqlite::strlit!(":memory:"));
-    if !db.is_ok() {
-        ctx.result_error(
-            "failed to open the in-memory db required to calculate schema modifications",
-        );
-        return;
-    }
-    // let db = db.unwrap();
+pub fn automigrate(db: &mut sqlite::Connection, schema: &str) -> Result<ResultCode, ResultCode> {
+    // extension might not be loaded in a runtime loadable extension environment
+    // so we'd need to drop all `crsql_as_crr` statements. Re-write them to no-ops?
+    let tempdb = sqlite::open(sqlite::strlit!(":memory:"))?;
 
     /*
      * The automigrate algorithm:
@@ -49,30 +26,4 @@ pub extern "C" fn crsql_automigrate(
      *   -- well we could if only type information on the columns changed or primary key participation changed
      *   -- need to also figure out index changes
      */
-    ctx.result_text_owned(String::from("ello mate!"));
-}
-
-// fn pull_schema_version(schema: &str) {
-//     // pull first line
-// }
-
-#[no_mangle]
-pub extern "C" fn sqlite3_crsqlautomigrate_init(
-    db: *mut sqlite::sqlite3,
-    _err_msg: *mut *mut c_char,
-    api: *mut sqlite::api_routines,
-) -> u32 {
-    sqlite::EXTENSION_INIT2(api);
-
-    db.create_function_v2(
-        "crsql_automigrate",
-        1,
-        sqlite::UTF8,
-        None,
-        Some(crsql_automigrate),
-        None,
-        None,
-        None,
-    )
-    .unwrap_or(sqlite::ResultCode::ERROR) as u32
 }
--- rs/automigrate-core/src/lib.rs
+++ rs/automigrate-core/src/lib.rs
@@ -1,4 +1,61 @@
 #![no_std]
 mod automigrate;
+extern crate alloc;
+use alloc::string::String;
+use core::ffi::c_char;
+use core::slice;
+use sqlite_nostd as sqlite;
+
+use sqlite::Connection;
+use sqlite::Context;
+use sqlite::Value;
 
 pub use automigrate::*;
+
+pub extern "C" fn crsql_automigrate(
+    ctx: *mut sqlite::context,
+    argc: i32,
+    argv: *mut *mut sqlite::value,
+) {
+    let args = sqlite::args!(argc, argv);
+    if argc != 1 {
+        ctx.result_error("expected 1 argument");
+        return;
+    }
+
+    // todo: start save pont
+    let result = automigrate::automigrate(db, args[0].text());
+    if let Err(err) = result {
+        // todo: rollback savepoint
+        ctx.result_error("failed to migrate");
+        return;
+    }
+    // todo: commit savepoint
+
+    ctx.result_text_owned(String::from("migrated"));
+}
+
+// fn pull_schema_version(schema: &str) {
+//     // pull first line
+// }
+
+#[no_mangle]
+pub extern "C" fn sqlite3_crsqlautomigrate_init(
+    db: *mut sqlite::sqlite3,
+    _err_msg: *mut *mut c_char,
+    api: *mut sqlite::api_routines,
+) -> u32 {
+    sqlite::EXTENSION_INIT2(api);
+
+    db.create_function_v2(
+        "crsql_automigrate",
+        1,
+        sqlite::UTF8,
+        None,
+        Some(crsql_automigrate),
+        None,
+        None,
+        None,
+    )
+    .unwrap_or(sqlite::ResultCode::ERROR) as u32
+}
